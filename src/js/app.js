dd7697b0748bb6401af09467111dae63408ad33a
salihcandusmez <salihcandusmez@gmail.com>
2018-11-05 23:35:44 +0300

    inheriting constructors started

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
+class Vehicle {
 
-class Drone {
-  constructor (id, name) {
-    this.id = id
-    this.name = name
-  }
 }
 
-let drone = new Drone('A123', 'Salo')
-let drone2 = new Drone('B123', 'Can')
+class Drone extends Vehicle {
 
-console.log(`drone: ${drone2.id} ${drone2.name}`)
+}
+
+class Car extends Vehicle {
+
+}
